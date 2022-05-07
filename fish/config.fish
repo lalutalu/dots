cee2b86955b8fcdad25f26c476b8cc4ba1951fb2
lalutalu <98784748+lalutalu@users.noreply.github.com>
2022-05-07 09:38:33 -0400

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,50 @@
+# Starship Prompt
+starship init fish | source
+
+set fish_greeting
+
+### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+function fish_user_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
+end
+
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+# The bindings for !! and !$
+if [ $fish_key_bindings = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+alias v="nvim"
+alias e='./mnt/c/Program Files/Emacs/emacs-28.1/bin/runemacs.exe'
+alias cls=clear
+alias ls='exa -a --color=always --group-directories-first' # my preferred listing
+alias la='exa -al --color=always --group-directories-first'  # all files and dirs
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
